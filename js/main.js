2e46d005dd8daded94b01c59df43a1773809dfda
afaiz-space <afaizan100@gmail.com>
2026-04-26 15:05:20 +0530

    project upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const toggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+toggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+ 
+
